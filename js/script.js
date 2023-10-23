eda1dcbee7b92cc9097ebb54897f1a5a84b0ed58
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-23 16:30:53 +0200

    aggiunti membri del team in card sul DOM

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 <div class="col-4">
         <div class="card text-start">
-          <img class="card-img-top" src="img/cats/fuffy.jpg" alt="Title">
+          <img class="card-img-top" src="img/team/fuffy.jpg" alt="Title">
           <div class="card-body">
             <h4 class="card-title">Fuffy</h4>
             <p class="card-text">Caratteristiche</p>
@@ -17,41 +17,64 @@ const team = [
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
-        picture: "img/wayne-barnett-founder-ceo.jpg",
+        picture: "wayne-barnett-founder-ceo.jpg",
     },
     {
         name: "Angela Caroll",
         role: "Chief Editor",
-        picture: "img/angela-caroll-chief-editor.jpg",
+        picture: "angela-caroll-chief-editor.jpg",
     },
     {
         name: "Walter Gordon",
         role: "Office Manager",
-        picture: "img/walter-gordon-office-manager.jpg",
+        picture: "walter-gordon-office-manager.jpg",
     },
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
-        picture: "img/angela-lopez-social-media-manager.jpg",
+        picture: "angela-lopez-social-media-manager.jpg",
     },
     {
         name: "Scott Estrada",
         role: "Developer",
-        picture: "img/scott-estrada-developer.jpg",
+        picture: "scott-estrada-developer.jpg",
     },
     {
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        picture: "img/barbara-ramos-graphic-designer.jpg",
+        picture: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
-console.log(team[0].name, team[0].role, team[0].picture)
-console.log(team[1].name, team[1].role, team[1].picture)
-console.log(team[2].name, team[2].role, team[2].picture)
-console.log(team[3].name, team[3].role, team[3].picture)
-console.log(team[4].name, team[4].role, team[4].picture)
-console.log(team[5].name, team[5].role, team[5].picture)
+// console.log(team[0].name, team[0].role, team[0].picture)
+// console.log(team[1].name, team[1].role, team[1].picture)
+// console.log(team[2].name, team[2].role, team[2].picture)
+// console.log(team[3].name, team[3].role, team[3].picture)
+// console.log(team[4].name, team[4].role, team[4].picture)
+// console.log(team[5].name, team[5].role, team[5].picture)
 
+const rowEl = document.querySelector(".row");
+console.log(rowEl);
 
+for (let i = 0; i < team.length; i++) {
+    printCol(team[i]);
+}
 
+function printCol(member) {
+    const col = document.createElement("div");
+    col.classList.add("col-4");
+    const template = `
+          <div class="card text-start my-2">
+            <img class="card-img-top" src="img/${member.picture}" alt="Title">
+            <div class="card-body">
+              <h4 class="card-title text-center">${member.name}</h4>
+              <p class="card-text text-center">
+              ${member.role}
+              </p>
+              </div>
+          </div>
+      `;
+    col.innerHTML = template;
+
+    rowEl.append(col);
+}
