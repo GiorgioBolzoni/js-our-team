fb4debffcb0840fe6f0dc60e62f2f32087054a98
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-23 15:48:30 +0200

    creato array con elementi tabella

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/*
+
+<div class="col-4">
+        <div class="card text-start">
+          <img class="card-img-top" src="img/cats/fuffy.jpg" alt="Title">
+          <div class="card-body">
+            <h4 class="card-title">Fuffy</h4>
+            <p class="card-text">Caratteristiche</p>
+          </div>
+        </div>
+</div>
+*/
+
+"use strict";
+
+const team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "img/scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "img/barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+
+
+
+
